f0f83f9de10d6c3ffad46a169c29e05cb6dce202
wr786 <wr786@pku.edu.cn>
2020-02-19 12:21:14 +0800

    20200219

---
+++ src/编程填空：左边i位取反.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int bitManipulation3(int n, int i) {
+  return n ^ (int)((long long)(1<<32) - (1<<(32-i)));
+}
+
+int main() {
+	int t, n, i;
+	cin >> t;
+	while (t--) {
+		cin >> n >> i;
+		cout << bitManipulation3(n, i) << endl;
+	}
+	return 0;
+}
